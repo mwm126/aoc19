a7b71e0cd0f74241b8c009f3560be006d7ed0d60
Mark Meredith <mark.meredith@netl.doe.gov>
2019-12-01 11:18:39 -0500

    Second star

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,28 +1,54 @@
 use std::io::BufRead;
 
 fn main() {
-    let input = std::fs::File::open("input-1.txt").unwrap();
+    let input = std::fs::File::open("../input-1.txt").unwrap();
     let fuel_total: i32 = std::io::BufReader::new(input)
         .lines()
         .map(|line| line.unwrap().parse::<i32>().unwrap())
         .map(fuel)
         .sum();
     println!("Total fuel requirement: {}", fuel_total);
+
+    let input = std::fs::File::open("../input-1.txt").unwrap();
+    let fuel_total2: i32 = std::io::BufReader::new(input)
+        .lines()
+        .map(|line| line.unwrap().parse::<i32>().unwrap())
+        .map(fuel2)
+        .sum();
+    println!("Total fuel requirement: {}", fuel_total2);
 }
 
 fn fuel(mass: i32) -> i32 {
     mass / 3 - 2
 }
 
+fn fuel2(mass: i32) -> i32 {
+    let f = fuel(mass);
+    if f<=0 {
+        0
+    } else {
+        f + fuel2(mass / 3 - 2)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::fuel;
+    use super::fuel2;
 
     #[test]
-    fn it_works() {
+    fn just_cargo() {
         assert_eq!(fuel(12), 2);
         assert_eq!(fuel(14), 2);
         assert_eq!(fuel(1969), 654);
         assert_eq!(fuel(100_756), 33583);
+    }
+
+    #[test]
+    fn cargo_plus_fuel() {
+        assert_eq!(fuel2(12), 2);
+        assert_eq!(fuel2(14), 2);
+        assert_eq!(fuel2(1969), 966);
+        assert_eq!(fuel2(100_756), 50346);
     }
 }
