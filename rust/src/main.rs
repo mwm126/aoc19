ea03253a255deb3ea83598edc013d333c1f6c09c
Mark Meredith <mark.meredith@netl.doe.gov>
2019-12-05 16:45:40 -0500

    Day 5 (so far)

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,10 +2,12 @@ mod aoc_1;
 mod aoc_2;
 mod aoc_3;
 mod aoc_4;
+mod aoc_5;
 
 fn main() {
     aoc_1::aoc_1();
     aoc_2::aoc_2();
     aoc_3::aoc_3();
     aoc_4::aoc_4();
+    aoc_5::aoc_5();
 }
